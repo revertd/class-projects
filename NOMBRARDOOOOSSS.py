dae6e458b9fe16d7a0944b6f537caa7b3a44345f
revert <109316946+revertd@users.noreply.github.com>
2024-05-03 12:09:34 -0400

    Update

---
+++ NOMBRARDOOOOSSS.py
@@ -0,0 +1,13 @@
+#Crea una lista con cinco nombres ingresados por teclado
+
+print("Ingresa 5 nombres: ")
+
+a = input("Ingresa el primer nombre: ")
+b = input("Ingresa el segundo nombre: ")
+c = input("Ingresa el tercer nombre: ")
+d = input("Ingresa el cuarto nombre: ")
+e = input("Ingresa el quinto nombre: ")
+
+z = [a,b,c,d,e]
+
+print(z)
