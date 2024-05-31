842924ae7cc15afca7ac8734b5bcb518c397314a
revert <109316946+revertd@users.noreply.github.com>
2024-05-31 12:56:40 -0400

    Create #DIENOMBRELISTAARDAOOOOOOOOOOOOOOOOO.py

---
+++ #DIENOMBRELISTAARDAOOOOOOOOOOOOOOOOO.py
@@ -0,0 +1,11 @@
+#crea una codigo que nos permita ingresar 10 nombres y guardalos en una lista
+con = 0
+l = []
+print("Este programa te ayuda a guardar 10 nombres en un lista.")
+while con < 10:
+    a = input("Ingresa un nombre: ")
+    con = con + 1
+    l.append(a)
+    print(l)
+    
+    
