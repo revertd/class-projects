5cd69c1c7b36f9f40d6132d01107cea45bd9a4fd
revert <109316946+revertd@users.noreply.github.com>
2024-07-11 18:24:14 +0200

    Create #CREARLOGIN.py

---
+++ #CREARLOGIN.py
@@ -0,0 +1,25 @@
+def crear_cuenta():
+    username = input("Ingresa un nombre de usuario: ")
+    
+    while True:
+         password = input("Ingresa una contraseña (minimo de 6 caracteres, con la letra inicial del nombre de usuario como primer caracter): ")
+         if len(password) < 6 or password [0] != username[0]:
+             print("La contraseña debe de tener minimo 6 caracteres y debe de tener la letra inicial del nombre de usuario como primer caracter")
+         else:
+             break
+    
+    print(f"Cuenta creada correctamente. Nombre de usuario {username}, Contraseña: {password} ")
+    return username, password
+    
+def iniciar_sesion(username, password):
+    user_input_username = input("Ingrese el nombre de usuario: ")
+    user_input_password = input("Ingrese la contraseña: ")
+    
+    if user_input_password == password and user_input_username == username:
+        print(f"Bienvenido {username}")
+    else:
+        
+        print("Nombre de usuario o contraseña incorrecta, vuelvelo a intentar: ")
+            
+username, password = crear_cuenta()
+iniciar_sesion(username, password)
